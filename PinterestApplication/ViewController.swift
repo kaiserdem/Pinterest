3466555eba8c660b8a27eead3756a75169927be8
Yaroslav Golinskiy <kaiserdem@gmail.com>
2019-08-22 22:02:39 +0300

    first commit, created proj, added collectionView

---
+++ PinterestApplication/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  PinterestApplication
+//
+//  Created by Kaiserdem on 22.08.2019.
+//  Copyright © 2019 Kaiserdem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  //MARK: Outlets
+  
+  @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+  @IBOutlet weak var collectionView: UICollectionView!
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
+// MARK: Data sourse
+
+extension ViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+    return cell
+  }
+  
+  
+}
