582eb81ec071cd81da2e19830ed7f12f96b7ff99
Yaroslav Golinskiy <kaiserdem@gmail.com>
2019-08-23 13:46:05 +0300

    setup collection view API and settings layout is finished

--- PinterestApplication/ViewController.swift
+++ PinterestApplication/ViewController.swift
@@ -14,25 +14,56 @@ class ViewController: UIViewController {
   
   @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
   @IBOutlet weak var collectionView: UICollectionView!
+  
+  //MARK: Properties
+  
+  let images = [#imageLiteral(resourceName: "image-1.jpg"),#imageLiteral(resourceName: "image-2.jpg"),#imageLiteral(resourceName: "image-3.jpg"),#imageLiteral(resourceName: "image-4.jpg"),#imageLiteral(resourceName: "image-5.jpg"),#imageLiteral(resourceName: "image-6.jpg"),#imageLiteral(resourceName: "image-7.jpg")]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    self.title = "Hello"
-    // Do any additional setup after loading the view, typically from a nib.
+    if let layout = collectionView.collectionViewLayout as? PinterestLayout {
+      layout.delegate = self
+    }
+    collectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    
   }
 
 
+}
+
+// MARK: Flow layout delegate
+extension ViewController: PinterestLayoutDelegate  {
+  func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: IndexPath) -> CGFloat {
+    let image = images[indexPath.item]
+    let height = image.size.height
+    
+    return height
+  }
+  
+//  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//    let numberOfColumns: CGFloat = 3
+//    let width = collectionView.frame.size.width
+//    let xInsets: CGFloat = 10
+//    let cellSpacing: CGFloat = 5
+//    let image = images[indexPath.item]
+//    let height = image.size.height
+//
+//    return CGSize(width: (width / numberOfColumns) - (xInsets + cellSpacing), height: height)
+//  }
 }
 
 // MARK: Data sourse
 
 extension ViewController: UICollectionViewDataSource {
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 10
+    return images.count
   }
   
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+    let image  = images[indexPath.item]
+    cell.imageView.image = image
     return cell
   }
   
---
+++ PinterestApplication/Views/PhotoCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCell.swift
+//  PinterestApplication
+//
+//  Created by Kaiserdem on 23.08.2019.
+//  Copyright © 2019 Kaiserdem. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+  
+  @IBOutlet weak var imageView: UIImageView!
+  
+}
